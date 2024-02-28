1a4d02a1b61d1de83f050e397a941567e798281d
Abhishek Verma <abhishekverm2018@gmail.com>
2024-02-28 22:24:54 +0530

    written lots of file

---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np
+import gym
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+env = gym.make('CartPole-v1')
+
+states = env.observation_space.shape[0]
+actions = env.action_space.n
+
+print(states)
+print(actions)
+
+model = Sequential()
+model.add(Flatten(input_shape=(1, states)))
+model.add(Dense(24, activation="relu"))
+model.add(Dense(24, activation="relu"))
+model.add(Dense(actions, activation="relu"))
+
+agent = DQNAgent(
+    model = model,
+    memory = SequentialMemory(limit=50000, window_length=1),
+    policy = BoltzmannQPolicy(),
+    nb_actions = actions,
+    nb_steps_warmup = 10,
+    target_model_update = 0.01
+)
+
+agent.compile(Adam(lr=0.001), metrics=['mae'])
+agent.fit(env, nb_steps=100000, visualize=False, verbose=1)
+
+results = agent.test(env, nb_episodes=10, visualize=True)
+print(np.mean(results.history["episode_reward"]))
+
+env.close()
+
